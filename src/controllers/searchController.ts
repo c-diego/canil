d1a49e470419489df33f81a62a7e46009748d34b
c-diego <diegocorte02@hotmail.com>
2023-07-23 19:16:01 -0300

    fix: redirecionando busca em branco para home

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -6,6 +6,11 @@ export const search = (req: Request, res: Response) => {
   const query = req.query.query as string;
   const pets = PetUseCase.getFromName(query);
 
+  if (!query) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('pages/page', {
     menu: createMenuObject(''),
     pets,
